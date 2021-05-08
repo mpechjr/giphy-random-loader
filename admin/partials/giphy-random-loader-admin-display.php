0b777f7b709dbe278e5e7c169d1f653dcc41b17f
melanio <melaniojr@pech.com.bz>
2021-05-08 10:55:36 -0600

    Update API field and Search field in form

--- admin/partials/giphy-random-loader-admin-display.php
+++ admin/partials/giphy-random-loader-admin-display.php
@@ -14,13 +14,47 @@
 
 ?>
 <?php 
- $giphy= get_option('giphy_search');
+
  
- ?>
+ //must check that the user has the required capability 
+ if (!current_user_can('manage_options'))
+ {
+   wp_die( __('You do not have sufficient permissions to access this page.') );
+ }
+
+ // variables for the field and option names 
+ $opt_api_name = 'giphy_api';
+ $opt_search_name = 'giphy_search';
+ $hidden_field_name = 'submit_hidden';
+ $data_field_api_name = 'giphy_api';
+ $data_field_name = 'giphy_search';
+ // Read in existing option value from database
+ 
+
+ // See if the user has posted us some information
+ // If they did, this hidden field will be set to 'Y'
+ if( isset($_POST[ $hidden_field_name ]) && $_POST[ $hidden_field_name ] == 'Y' ) {
+     // Read their posted value
+     $opt_val_api = $_POST[ $data_field_api_name ];
+     $opt_val_search = $_POST[ $data_field_name ];
+     // Save the posted value in the database
+     update_option( $opt_api_name, $opt_val_api );
+
+     // Save the posted value in the database
+     update_option( $opt_search_name, $opt_val_search );
+
+ }
+
+ $giphy_api= get_option($opt_api_name);
+ $giphy_search= get_option($opt_search_name);
+?>
 
 <div class="">
+<h1>Giphy Random Loader</h1>
 <form  name="form1"  method="post" action="">
-<input type="text" name="giphy_search" value="<?php echo $giphy ?>">
+<input type="hidden" name="<?php echo $hidden_field_name; ?>" value="Y">
+<div style="margin-bottom:1em;">API <input type="text" name="<?php echo $data_field_api_name ?>" value="<?php echo $giphy_api ?>"> </div>
+<div>Search terms <input type="text" name="<?php echo $data_field_name ?>" value="<?php echo $giphy_search ?>"></div>
 <input type="submit">
 </form>
 </div>
